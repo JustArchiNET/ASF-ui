4b91be0794dece44dd08ac3265cf1a63bbedc966
MrBurrBurr <31552675+MrBurrBurr@users.noreply.github.com>
2018-09-15 03:50:32 +0200

    Change container border color
    The border color of default container boxes will now use the theme color.

--- src/App.vue
+++ src/App.vue
@@ -39,7 +39,7 @@
         --color-text-dark: #{$color-text-dark};
         --color-text-secondary: #{darken($color-text, 10)};
         --color-text-disabled: #{darken($color-text, 25)};
-        --color-border: #{darken($color-text, 25)};
+        --color-border: #{$color-theme-blue};
         --color-background: #ecf0f5;
         --color-background-light: #fff;
         --color-navigation: #222d32;
