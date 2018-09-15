dc39789d49abbac3fb4de44033efd4a6619586cc
Arkadiusz Sygulski <arkadiusz@sygulski.pl>
2018-09-15 20:58:18 +0900

    Add command execution helper

--- src/utils/http.js
+++ src/utils/http.js
@@ -4,10 +4,18 @@ const http = axios.create({
   baseURL: '/Api'
 });
 
+function extractResult(response) {
+  return response.data.Result;
+}
+
 export function authenticate(password) {
   http.defaults.headers.common.Authentication = password;
 }
 
 export function get(endpoint) {
-  return http.get(endpoint).then(response => response.data.Result);
+  return http.get(endpoint).then(extractResult);
+}
+
+export function command(...args) {
+  return http.post(`Command/${args.join(' ')}`).then(extractResult);
 }
