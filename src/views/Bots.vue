70b08a7091a8ceb77350c11a0a9219815ab84f39
MrBurrBurr <31552675+MrBurrBurr@users.noreply.github.com>
2018-09-15 03:53:55 +0200

    Add new routes

---
+++ src/views/Bots.vue
@@ -0,0 +1,16 @@
+<template>
+    <main class="main-container">
+        <h2 class="title">Bots</h2>
+
+        <div class="container">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A deleniti dignissimos, ex hic laboriosam quibusdam reiciendis repellendus temporibus! Eius ipsam, repellat! Autem distinctio dolore doloribus eum facere fugit quo tempora!</p>
+        </div>
+    </main>
+</template>
+
+<script>
+  export default {
+    name: 'bots',
+    components: { }
+  };
+</script>
