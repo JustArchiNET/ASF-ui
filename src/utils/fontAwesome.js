8492870cc5bad6b8142379d6380e1241c81ac52b
MrBurrBurr <mail@FREDERICK.win>
2018-09-18 01:54:30 +0200

    Change icon for config editor

--- src/utils/fontAwesome.js
+++ src/utils/fontAwesome.js
@@ -2,10 +2,10 @@ import Vue from 'vue';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faWrench, faBars, faLaptop, faUsers, faFileAlt, faTachometerAlt, faHome, faPowerOff, faPause, faCogs, faClock,
-  faTimesCircle, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+  faTimesCircle, faCheckCircle, faEdit } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faWrench, faBars, faLaptop, faUsers, faFileAlt, faTachometerAlt, faHome, faPowerOff, faPause, faCogs, faClock,
-  faTimesCircle, faCheckCircle);
+  faTimesCircle, faCheckCircle, faEdit);
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 Vue.component('font-awesome-icon', FontAwesomeIcon);
