95997ac0660ad952fdce0aa8aae7e3e72e60096d
Abrynos <6608231+Abrynos@users.noreply.github.com>
2018-09-21 13:27:27 +0200

    Remove variable

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,12 +1,10 @@
 export function timeDifference(startTime, endTime) {
     let difference = (endTime - startTime) / 1000;
 
-    let result =  {
+    return {
         seconds: Math.floor(difference % 60),
         minutes: Math.floor(difference / 60 % 60),
         hours: Math.floor(difference / (60 * 60) % 24),
         days: Math.floor(difference / (24 * 60 * 60))
     };
-
-    return result;
 }
