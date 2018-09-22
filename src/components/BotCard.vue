581b883ae5295511918a3bdf23e1dc9281110049
SKANKHUNTER <31552675+MrBurrBurr@users.noreply.github.com>
2018-09-23 01:10:58 +0200

    Place v-if and v-else correctly

--- src/components/BotCard.vue
+++ src/components/BotCard.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="bot" :class="[`status--${bot.status}`]">
-    <a v-if="bot.steamid" target="_blank" :href="bot.profileURL">
+    <a target="_blank" :href="bot.profileURL" v-if="bot.steamid !== '0'">
       <img class="bot__avatar" :src="bot.avatarURL">
     </a>
     
-    <img v-else class="bot__avatar" :src="bot.avatarURL">
+    <img class="bot__avatar" :src="bot.avatarURL" v-else>
 
     <div class="bot__status">
       <router-link tag="span" :to="{ name: 'bot', params: { bot: bot.name } }" class="bot__status-property bot__status-property--name">{{ bot.name }}</router-link>
