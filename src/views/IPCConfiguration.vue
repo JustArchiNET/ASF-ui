5df68ae90482f8998f224fe73c0bbea837df22ac
Arkadiusz Sygulski <arkadiusz@sygulski.pl>
2018-09-24 22:08:50 +0200

    Remove base URL from config

--- src/views/IPCConfiguration.vue
+++ src/views/IPCConfiguration.vue
@@ -4,11 +4,6 @@
 			<h2 class="title">IPC Configuration</h2>
 
 			<div class="form-item">
-				<label for="baseURL" class="form-item__label">Base URL</label>
-				<input id="baseURL" class="form-item__input" type="text" v-model="baseURL" @keydown.enter="update">
-			</div>
-
-			<div class="form-item">
 				<label for="password" class="form-item__label">Password</label>
 				<input id="password" class="form-item__input" type="password" v-model="password" @keydown.enter="update">
 			</div>
@@ -27,24 +22,12 @@
 		components: {},
 		data() {
 			return {
-				password: this.$store.getters['auth/password'],
-				baseURL: this.$store.getters['auth/baseURL']
+				password: this.$store.getters['auth/password']
 			};
-		},
-		computed: {
-			normalizedBaseURL() {
-				return this.baseURL.endsWith('/') ? this.baseURL : this.baseURL + '/';
-			}
 		},
 		methods: {
 			async update() {
-				const baseURLChanged = this.normalizedBaseURL !== this.$store.getters['auth/baseURL'];
-
-				this.$store.commit('auth/setBaseURL', this.normalizedBaseURL);
 				this.$store.commit('auth/setPassword', this.password);
-
-				if (baseURLChanged) window.location.href = `${this.normalizedBaseURL}ipc-configuration`;
-
 				const validPassword = await this.$store.dispatch('auth/validate');
 				if (!validPassword) await this.$router.replace({ name: 'setup' });
 			}
