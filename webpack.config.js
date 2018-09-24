ceab8fcd8179d644f1241cc5a4dec431b2b1f20c
Arkadiusz Sygulski <arkadiusz@sygulski.pl>
2018-09-24 17:52:36 +0200

    Remove html-webpack-plugin as it's no longer used

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 const { VueLoaderPlugin } = require('vue-loader');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = async (env, argv) => {
