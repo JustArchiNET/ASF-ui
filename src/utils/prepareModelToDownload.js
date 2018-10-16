6c8ac09701257ac7d3eebacbbb4f2530923629c3
Arkadiusz Sygulski <arkadiusz@sygulski.pl>
2018-10-16 14:28:39 +0200

    Pretty print JSON when downloading config file, closes #38

--- src/utils/prepareModelToDownload.js
+++ src/utils/prepareModelToDownload.js
@@ -1,5 +1,5 @@
 const sPropertyRegex = /"s_(\w+)":\s*"(\d+)"/g;
 
 export default function prepareModelToDownload(model) {
-	return JSON.stringify(model).replace(sPropertyRegex, '"$1":$2');
+	return JSON.stringify(model, null, 2).replace(sPropertyRegex, '"$1":$2');
 }
