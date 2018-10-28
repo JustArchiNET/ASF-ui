e8465c3aeb035420c69a4b59a5fa3c2aa1e698a9
MrBurrBurr <31552675+MrBurrBurr@users.noreply.github.com>
2018-10-28 15:55:25 +0100

    Fix BEM

--- src/components/BotFarmingInfo.vue
+++ src/components/BotFarmingInfo.vue
@@ -3,25 +3,25 @@
 		<h2 class="title">{{ $t('farming-info') }}</h2>
 
 		<div class="farming__info">
-			<div class="farming__info-card">
-				<div class="farming__info-card--icon"><font-awesome-icon icon="gamepad"></font-awesome-icon></div>
-				<div class="farming__info-card-content">
-					<div class="farming__info-card--name">{{ $t('farming-info-games') }}</div>
-					<div class="farming__info-card--text">PLACEHOLDER</div>
+			<div class="farming__card">
+				<div class="farming__card-icon"><font-awesome-icon icon="gamepad"></font-awesome-icon></div>
+				<div class="farming__card-content">
+					<div class="farming__card-content--name">{{ $t('farming-info-games') }}</div>
+					<div class="farming__card-content--text">PLACEHOLDER</div>
 				</div>
 			</div>
-			<div class="farming__info-card">
-				<div class="farming__info-card--icon"><font-awesome-icon icon="clock"></font-awesome-icon></div>
-				<div class="farming__info-card-content">
-					<div class="farming__info-card--name">{{ $t('farming-info-time') }}</div>
-					<div class="farming__info-card--text">PLACEHOLDER</div>
+			<div class="farming__card">
+				<div class="farming__card-icon"><font-awesome-icon icon="clock"></font-awesome-icon></div>
+				<div class="farming__card-content">
+					<div class="farming__card-content--name">{{ $t('farming-info-time') }}</div>
+					<div class="farming__card-content--text">PLACEHOLDER</div>
 				</div>
 			</div>
-			<div class="farming__info-card">
-				<div class="farming__info-card--icon"><font-awesome-icon icon="clone"></font-awesome-icon></div>
-				<div class="farming__info-card-content">
-					<div class="farming__info-card--name">{{ $t('farming-info-cards') }}</div>
-					<div class="farming__info-card--text">PLACEHOLDER</div>
+			<div class="farming__card">
+				<div class="farming__card-icon"><font-awesome-icon icon="clone"></font-awesome-icon></div>
+				<div class="farming__card-content">
+					<div class="farming__card-content--name">{{ $t('farming-info-cards') }}</div>
+					<div class="farming__card-content--text">PLACEHOLDER</div>
 				</div>
 			</div>
 		</div>
@@ -42,12 +42,12 @@
 		justify-content: space-between;
 	}
 
-	.farming__info-card {
+	.farming__card {
 		background: var(--color-background-light);
 		display: flex;
 	}
 
-	.farming__info-card--icon {
+	.farming__card-icon {
 		height: 4rem;
 		width: 4rem;
 		font-size: 2.1em;
@@ -58,19 +58,19 @@
 		color: var(--color-text);
 	}
 
-	.farming__info-card-content {
+	.farming__card-content {
 		display: flex;
 		flex-direction: column;
 		overflow: hidden;
 		padding: 5px 10px;
 	}
 
-	.farming__info-card--name {
+	.farming__card-content--name {
 		text-transform: uppercase;
 		font-size: 0.9em;
 	}
 
-	.farming__info-card--text {
+	.farming__card-content--text {
 		font-weight: 600;
 		font-size: 1.2em;
 		padding-top: 10px;
