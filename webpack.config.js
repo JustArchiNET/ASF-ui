848ccd3fa84344f87eb63ee606f2445f6d04f486
JustArchi <JustArchi@JustArchi.net>
2018-10-31 01:27:47 +0100

    Revert "Add Sentry.io integration for automatic error reporting"
    This reverts commit 324f7755b9b1d8986b74980fe7a9d9b13a10ab28.
    
    Minimal requirements for this to happen:
    
    > automatic reporting + default true + option to turn off + notification on start

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,6 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const TerserWebpackPlugin = require('terser-webpack-plugin');
-const DefinePlugin = require('webpack').DefinePlugin;
-const GitRevisionPlugin = require('git-revision-webpack-plugin');
-
-const gitRevisionPlugin = new GitRevisionPlugin();
 
 module.exports = async (env, argv) => {
 	const isProd = env === 'production';
@@ -77,10 +73,7 @@ module.exports = async (env, argv) => {
 		plugins: [
 			new CleanWebpackPlugin(['dist']),
 			new VueLoaderPlugin(),
-			new CopyWebpackPlugin(['src/include']),
-			new DefinePlugin({
-				COMMITHASH: JSON.stringify(gitRevisionPlugin.commithash())
-			})
+			new CopyWebpackPlugin(['src/include'])
 		],
 		optimization: {
 
