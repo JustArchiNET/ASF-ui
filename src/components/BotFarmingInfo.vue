859d05ba74ee86f63d83cbaac5ffe3e6ca8fb91f
Arkadiusz Sygulski <arkadiusz@sygulski.pl>
2018-11-01 02:38:11 +0100

    Improve responsiveness

--- src/components/BotFarmingInfo.vue
+++ src/components/BotFarmingInfo.vue
@@ -6,23 +6,23 @@
 			<div class="info-card">
 				<div class="info-card__icon"><font-awesome-icon icon="gamepad"></font-awesome-icon></div>
 				<div class="info-card__body">
-					<div class="info-card__title">{{ $t('farming-info-games') }}</div>
-					<div class="info-card__value">PLACEHOLDER</div>
+					<p class="info-card__title">{{ $t('farming-info-games') }}</p>
+					<p class="info-card__value">PLACEHOLDER</p>
 				</div>
 			</div>
 
 			<div class="info-card">
 				<div class="info-card__icon"><font-awesome-icon icon="clock"></font-awesome-icon></div>
 				<div class="info-card__body">
-					<div class="info-card__title">{{ $t('farming-info-time') }}</div>
-					<div class="info-card__value">PLACEHOLDER</div>
+					<p class="info-card__title">{{ $t('farming-info-time') }}</p>
+					<p class="info-card__value">PLACEHOLDER</p>
 				</div>
 			</div>
 			<div class="info-card">
 				<div class="info-card__icon"><font-awesome-icon icon="clone"></font-awesome-icon></div>
 				<div class="info-card__body">
-					<div class="info-card__title">{{ $t('farming-info-cards') }}</div>
-					<div class="info-card__value">PLACEHOLDER</div>
+					<p class="info-card__title">{{ $t('farming-info-cards') }}</p>
+					<p class="info-card__value">PLACEHOLDER</p>
 				</div>
 			</div>
 		</div>
@@ -39,13 +39,17 @@
 	.info-cards {
 		display: grid;
 		grid-gap: 1em;
-		grid-template-columns: 1fr 1fr 1fr;
-		justify-content: space-between;
+		grid-template-columns: repeat(3, 1fr);
+
+		@media screen and (max-width: 750px) {
+			grid-template-columns: 1fr;
+		}
 	}
 
 	.info-card {
 		background: var(--color-background-light);
-		display: flex;
+		display: grid;
+		grid-template-columns: auto 1fr;
 	}
 
 	.info-card__icon {
@@ -64,16 +68,18 @@
 		flex-direction: column;
 		overflow: hidden;
 		padding: 5px 10px;
+		justify-content: space-between;
 	}
 
 	.info-card__title {
 		text-transform: uppercase;
 		font-size: 0.9em;
+		margin: 0;
 	}
 
 	.info-card__value {
 		font-weight: 600;
 		font-size: 1.2em;
-		padding-top: 10px;
+		margin: 0;
 	}
 </style>
