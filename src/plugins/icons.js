2099a11a1cb5f884da927bf19ecc0c987c877ea8
MrBurrBurr <31552675+MrBurrBurr@users.noreply.github.com>
2018-11-07 00:42:29 +0100

    Use same icon for cardsRemaining

--- src/plugins/icons.js
+++ src/plugins/icons.js
@@ -3,12 +3,12 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import {
 	faWrench, faBars, faLaptop, faUsers, faFileAlt, faTachometerAlt, faHome, faPowerOff, faPause, faCogs, faClock,
 	faTimesCircle, faCheckCircle, faEdit, faTimes, faSquare, faMoon, faPalette, faPlay, faQuestion, faPlus, faSpinner,
-	faKey, faTrash, faCloudDownloadAlt, faSignOutAlt, faAngleDown, faLanguage, faGamepad, faClone, faClipboardList
+	faKey, faTrash, faCloudDownloadAlt, faSignOutAlt, faAngleDown, faLanguage, faGamepad, faClone
 } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faWrench, faBars, faLaptop, faUsers, faFileAlt, faTachometerAlt, faHome, faPowerOff, faPause, faCogs, faClock,
 	faTimesCircle, faCheckCircle, faEdit, faTimes, faSquare, faMoon, faPalette, faPlay, faQuestion, faPlus, faSpinner,
-	faKey, faTrash, faCloudDownloadAlt, faSignOutAlt, faAngleDown, faLanguage, faGamepad, faClone, faClipboardList);
+	faKey, faTrash, faCloudDownloadAlt, faSignOutAlt, faAngleDown, faLanguage, faGamepad, faClone);
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
