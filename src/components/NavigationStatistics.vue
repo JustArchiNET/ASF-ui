7ad260a2be04434ac8ac0331361e79a98e8ff1cc
SKANKHUNTER <31552675+MrBurrBurr@users.noreply.github.com>
2018-11-07 01:03:04 +0100

    Only show cardsRemaining if we are farming (#114)
    * Only show cardsRemaining if we are farming
    
    * Use same icon for cardsRemaining

--- src/components/NavigationStatistics.vue
+++ src/components/NavigationStatistics.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="navigation-statistics">
-		<navigation-statistic :name="$t('farming-info-cards')" icon="clipboard-list" :value="cardsRemaining"></navigation-statistic>
+		<navigation-statistic v-if="cardsRemaining !== 0" :name="$t('farming-info-cards')" icon="clone" :value="cardsRemaining"></navigation-statistic>
 		<navigation-statistic :name="$t('statistics-memory-usage')" icon="tachometer-alt" :value="memory"></navigation-statistic>
 		<navigation-statistic :name="$t('statistics-uptime')" icon="clock" :value="uptime"></navigation-statistic>
 	</div>
