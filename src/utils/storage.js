8c3db9e1519951bdff4b9e886c781bef0d76966d
Arkadiusz Sygulski <arkadiusz@sygulski.pl>
2018-11-08 23:29:31 +0100

    JSON.stringify localStorage values

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -3,11 +3,14 @@ function generateKey(key) {
 }
 
 export function set(key, value) {
-	return localStorage.setItem(generateKey(key), value);
+	return localStorage.setItem(generateKey(key), JSON.stringify(value));
 }
 
 export function get(key, defaultValue) {
-	return localStorage.getItem(generateKey(key)) || defaultValue;
+	const storedValue = localStorage.getItem(generateKey(key));
+	if (!storedValue) return defaultValue;
+	try { return JSON.parse(storedValue) }
+	catch (err) { return storedValue; }
 }
 
 export function remove(key) {
