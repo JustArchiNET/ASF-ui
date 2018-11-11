9cd403bd1e08c41a51068ec8e3e22b41226dfe40
MrBurrBurr <31552675+MrBurrBurr@users.noreply.github.com>
2018-11-11 05:22:56 +0100

    Add option to UI Configuration

--- src/views/UIConfiguration.vue
+++ src/views/UIConfiguration.vue
@@ -35,6 +35,7 @@
 		data() {
 			const categories = [
 				{ name: this.$t('general'), fields: [this.$t('default-page')] },
+				{ name: this.$t('bots'), fields: [this.$t('show-bots')] },
 				{ name: this.$t('debug'), fields: [this.$t('logging'), this.$t('reporting')] }
 			];
 
@@ -50,7 +51,23 @@
 						[this.$t('bots')]: 'bots',
 						[this.$t('log')]: 'log',
 						[this.$t('last-visited-page')]: '_last-visited-page'
-					}
+					},
+					description: this.$t('default-page-description')
+				},
+				{
+					param: this.$t('show-bots'),
+					paramName: 'showBots',
+					type: 'flag',
+					defaultValue: 15,
+					values: {
+						[this.$t('all')]: 15,
+						[this.$t('disabled')]: 1,
+						[this.$t('offline')]: 2,
+						[this.$t('online')]: 4,
+						[this.$t('farming')]: 8
+
+					},
+					description: this.$t('show-bots-description')
 				},
 				{
 					param: this.$t('logging'),
@@ -71,6 +88,7 @@
 				categories,
 				model: {
 					defaultView: storage.get('settings:default-view'),
+					showBots: storage.get('settings:show-bots'),
 					sentryInstalled: this.$sentry.installed,
 					sentryReporting: this.$sentry.reporting
 				},
@@ -86,6 +104,7 @@
 			save() {
 				const model = this.model;
 				if (model.defaultView) storage.set('settings:default-view', model.defaultView);
+				if (model.showBots) storage.set('settings:show-bots', model.showBots);
 				model.sentryInstalled ? this.$sentry.install() : this.$sentry.destroy();
 				model.sentryReporting ? this.$sentry.enableReporting() : this.$sentry.disableReporting();
 				this.$success(this.$t('settings-saved'));
