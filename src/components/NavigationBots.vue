20b26faf2f3c23482573fedf5a2b01e3ea7e5fe5
Arkadiusz Sygulski <arkadiusz@sygulski.pl>
2018-11-13 10:29:06 +0100

    Make sure bot count always fits the space, closes #160

--- src/components/NavigationBots.vue
+++ src/components/NavigationBots.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="bot-cards">
 		<div class="bot-card" :class="[`status--${type}`]" v-for="type in botTypes">
-			<span class="bot-card__value">{{ count(type) }}</span>
+			<fit-text ref="count" :max="1.2" class="bot-card__value">{{ count(type) }}</fit-text>
 			<span class="bot-card__name">{{ $t(`bot-status-${type}`) }}</span>
 		</div>
 	</div>
@@ -9,21 +9,37 @@
 
 <script>
 	import { mapGetters } from 'vuex';
+	import FitText from './utils/FitText.vue';
 
 	export default {
 		name: 'navigation-bots',
-		computed: {
-			...mapGetters({
-				botsCount: 'bots/count'
-			}),
-			count() {
-				return type => Math.min(999, this.botsCount(type));
-			}
-		},
+		components: { FitText },
 		data() {
 			return {
 				botTypes: ['farming', 'online', 'offline', 'disabled']
 			};
+		},
+		computed: {
+			...mapGetters({
+				botsCount: 'bots/count',
+			}),
+			count() {
+				return type => this.botsCount(type);
+			}
+		},
+		methods: {
+			recalculateText() {
+				this.$refs.count.forEach(fitText => fitText.calculate());
+				if (this.transitioning) requestAnimationFrame(() => this.recalculateText());
+			},
+			onTransitionStart() {
+				if (this.transitioning) return;
+				this.transitioning = true;
+				this.recalculateText();
+			},
+			onTransitionEnd() {
+				this.transitioning = false;
+			}
 		}
 	};
 </script>
@@ -45,6 +61,7 @@
 	}
 
 	.bot-card {
+		min-width: 0;
 		padding: 0.25em 0.1em;
 		display: flex;
 		flex-direction: column;
@@ -53,9 +70,11 @@
 		color: var(--color-text);
 		text-shadow: 0 0 1px var(--color-text-dark);
 		background: var(--color-status);
+		overflow: hidden;
 	}
 
 	.bot-card__value {
+		word-break: break-all;
 		font-weight: 600;
 		font-size: 1.2em;
 	}
---
+++ src/components/utils/FitText.vue
@@ -0,0 +1,72 @@
+<template>
+  <span>
+    <slot></slot>
+  </span>
+</template>
+
+<script>
+	// https://github.com/robbeman/vue-fit-text
+
+	export default {
+		name: 'fit-text',
+		props: {
+			targetLineCount: {
+				'default': 1,
+				type: Number,
+			},
+			unit: {
+				'default': 'em',
+				type: String,
+			},
+			min: {
+				'default': 0.5,
+				type: Number,
+			},
+			max: {
+				'default': 1,
+				type: Number,
+			}
+		},
+		data() {
+			return {
+				observer: null,
+			}
+		},
+		methods: {
+			calculate() {
+				let element = this.$el;
+				// first make it an inline block and set the line height to a fixed pixel value
+				element.style.display = 'inline-block';
+				element.style.lineHeight = '1px';
+				// then keep trying untill it fits
+				let fontSize = this.max;
+				let stepSize = (this.unit === 'px') ? 1 : 0.05;
+				element.style.fontSize = fontSize + this.unit;
+				while (element.offsetHeight > this.targetLineCount && fontSize > this.min) {
+					fontSize -= stepSize;
+					element.style.fontSize = fontSize + this.unit;
+				}
+				// found it!!
+				// reset the styles
+				element.style.display = null;
+				element.style.lineHeight = null;
+			}
+		},
+		mounted() {
+			this.calculate();
+			if ('MutationObserver' in window && this.observer === null) {
+				// Create the observer (and what to do on changes...)
+				this.observer = new MutationObserver(this.calculate);
+				// Setup the observer
+				this.observer.observe(
+						this.$el,
+						{ subtree: true, characterData: true }
+				);
+			}
+		},
+		beforeDestroy: function() {
+			// Clean up
+			this.observer.disconnect();
+		}
+	}
+</script>
