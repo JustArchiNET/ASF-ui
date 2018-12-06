5cd467877dab95728cb88a497567d04b55534948
Arkadiusz Sygulski <arkadiusz@sygulski.pl>
2018-12-06 21:07:56 +0100

    Handle network errors properly

--- src/utils/getStatus.js
+++ src/utils/getStatus.js
@@ -11,6 +11,7 @@ export async function getStatus() {
 	return http.get('ASF')
 		.then(response => STATUS.AUTHENTICATED)
 		.catch(err => {
+			if (!err.response) return STATUS.NOT_CONNECTED;
 			if (err.response.status === 401) return STATUS.UNAUTHORIZED;
 			if (err.response.status === 403) return STATUS.RATE_LIMITED;
 			return STATUS.NOT_CONNECTED;
