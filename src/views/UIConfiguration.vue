90d0a2040082cbddea6af1dd5684433e286e5be3
MrBurrBurr <31552675+MrBurrBurr@users.noreply.github.com>
2019-01-22 00:44:48 +0100

    Rename `capture-snapshot`

--- src/views/UIConfiguration.vue
+++ src/views/UIConfiguration.vue
@@ -10,7 +10,7 @@
 					<button class="button button--confirm" @click="save">{{ $t('save') }}</button>
 
 					<dropdown :label="$t('debug')" class="button--confirm pull-right" :disabled="!sentryInstalled">
-						<li class="dropdown__item" @click="captureSnapshot">{{ $t('capture-snapshot') }}</li>
+						<li class="dropdown__item" @click="captureSnapshot">{{ $t('snapshot-capture') }}</li>
 						<li class="dropdown__item" :class="{ 'dropdown__item--disabled': !storedEventsCount }" @click="copyStoredEvents">{{ $t('copy-log') }}</li>
 					</dropdown>
 				</div>
