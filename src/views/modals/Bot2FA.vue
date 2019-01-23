8a1696999d5438de78b6f81c2d935099a020d079
Arkadiusz Sygulski <arkadiusz@sygulski.pl>
2019-01-23 14:58:20 +0100

    Add logic to accept / decline all 2fa confirmations

--- src/views/modals/Bot2FA.vue
+++ src/views/modals/Bot2FA.vue
@@ -4,16 +4,6 @@
 		<h2 class="title" v-else>{{ bot.name }}</h2>
 
 		<div class="form-item">
-			<input class="token" type="text" placeholder="-">
-			<button class="button" @click="refreshToken" :title="$t('token-refresh')">
-				<font-awesome-icon icon="sync-alt"></font-awesome-icon>
-			</button>
-			<button class="button" @click="copyToken" :title="$t('token-copy')">
-				<font-awesome-icon icon="clipboard"></font-awesome-icon>
-			</button>
-		</div>
-
-		<div class="form-item">
 			<div class="form-item__buttons form-item__buttons--center">
 				<button class="button button--confirm" @click="acceptTrades">{{ $t('2fa-ok') }}</button>
 				<button class="button button--cancel" @click="declineTrades">{{ $t('2fa-no') }}</button>
@@ -24,15 +14,10 @@
 
 <script>
 	import { mapGetters } from 'vuex';
-	import * as copy from 'copy-to-clipboard';
+	import { command } from '../../plugins/http';
 
 	export default {
 		name: 'bot-2fa',
-		components: {},
-		data() {
-			return {
-			};
-		},
 		computed: {
 			...mapGetters({
 				nicknames: 'settings/nicknames'
@@ -45,31 +30,19 @@
 			if (!this.bot) this.$router.replace({ name: 'bots' });
 		},
 		methods: {
-			refreshToken() {
+			async acceptTrades() {
+				const result = await command('2faok', this.bot.name);
+				this.$info(this.parseResultString(result));
 			},
-			copyToken() {
-				let token = 'D3XGP'; //just for testing
-
-				copy(token);
-				this.$info(this.$t('token-copied'));
+			async declineTrades() {
+				const result = await command('2fano', this.bot.name);
+				this.$info(this.parseResultString(result));
 			},
-			acceptTrades() {
-			},
-			declineTrades() {
+			parseResultString(result) {
+				return result
+						.replace(`<${this.bot.name}> `, '') // Remove bot name
+						.replace('<ASF> ', ''); // Remove <ASF> if the bot doesn't exist
 			}
 		}
 	};
 </script>
-
-<style lang="scss">
-	.token{
-		background: var(--color-background-light);
-		font-size: 24px;
-		width: 4.5em;
-		text-align: center;
-		vertical-align: middle;
-		height: 2.66667em;
-		border: 1px solid black;
-		border-radius: .1875em;
-	}
-</style>
