2da9da83cf0dd348ff03c014a4239d4c9d8bc95b
MrBurrBurr <31552675+MrBurrBurr@users.noreply.github.com>
2019-01-24 23:14:03 +0100

    Update icons.js

--- src/plugins/icons.js
+++ src/plugins/icons.js
@@ -1,16 +1,16 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 
 import {
-	faWrench, faBars, faLaptop, faUsers, faFileAlt, faTachometerAlt, faHome, faPowerOff, faPause, faCogs, faClock,
-	faTimesCircle, faCheckCircle, faEdit, faTimes, faSquare, faMoon, faPalette, faPlay, faQuestion, faPlus, faSpinner,
-	faKey, faTrash, faCloudDownloadAlt, faSignOutAlt, faAngleDown, faLanguage, faGamepad, faClone, faCalendarCheck,
-	faLock, faSyncAlt, faClipboard
+	faWrench, faBars, faLaptop, faUsers, faFileAlt, faTachometerAlt, faPowerOff, faPause, faCogs, faClock,
+	faTimesCircle, faCheckCircle, faEdit, faTimes, faSquare, faMoon, faPalette, faPlay, faQuestion, faPlus,
+	faSpinner, faKey, faTrash, faCloudDownloadAlt, faSignOutAlt, faAngleDown, faLanguage, faGamepad, faClone,
+	faCalendarCheck, faLock
 } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faWrench, faBars, faLaptop, faUsers, faFileAlt, faTachometerAlt, faHome, faPowerOff, faPause, faCogs, faClock,
-	faTimesCircle, faCheckCircle, faEdit, faTimes, faSquare, faMoon, faPalette, faPlay, faQuestion, faPlus, faSpinner,
-	faKey, faTrash, faCloudDownloadAlt, faSignOutAlt, faAngleDown, faLanguage, faGamepad, faClone, faCalendarCheck,
-	faLock, faSyncAlt, faClipboard);
+library.add(faWrench, faBars, faLaptop, faUsers, faFileAlt, faTachometerAlt, faPowerOff, faPause, faCogs,
+	faClock, faTimesCircle, faCheckCircle, faEdit, faTimes, faSquare, faMoon, faPalette, faPlay, faQuestion,
+	faPlus, faSpinner, faKey, faTrash, faCloudDownloadAlt, faSignOutAlt, faAngleDown, faLanguage, faGamepad,
+	faClone, faCalendarCheck, faLock);
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
