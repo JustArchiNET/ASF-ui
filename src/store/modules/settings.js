7dfba3c1ccd8a801ae76d692f3a1cc1f55b4c71d
MrBurrBurr <31552675+MrBurrBurr@users.noreply.github.com>
2019-01-26 22:58:41 +0100

    Remove UI Configuration "Hide bots"

--- src/store/modules/settings.js
+++ src/store/modules/settings.js
@@ -2,7 +2,6 @@ import * as storage from '../../utils/storage';
 
 export const state = {
 	defaultView: 'home',
-	hiddenBots: 0,
 	nicknames: false,
 	sentryInstalled: false,
 	sentryReporting: false
@@ -10,7 +9,6 @@ export const state = {
 
 export const mutations = {
 	setDefaultView: (state, defaultView) => state.defaultView = defaultView,
-	setHiddenBots: (state, hiddenBots) => state.hiddenBots = hiddenBots,
 	setNicknames: (state, nicknames) => state.nicknames = nicknames,
 	setSentryInstalled: (state, sentryInstalled) => state.sentryInstalled = sentryInstalled,
 	setSentryReporting: (state, sentryReporting) => state.sentryReporting = sentryReporting
@@ -19,7 +17,6 @@ export const mutations = {
 export const actions = {
 	init({ commit }) {
 		commit('setDefaultView', storage.get('settings:default-view', 'home'));
-		commit('setHiddenBots', storage.get('settings:hidden-bots', 0));
 		commit('setNicknames', storage.get('settings:nicknames', false));
 		commit('setSentryInstalled', storage.get('settings:sentry-installed', false));
 		commit('setSentryReporting', storage.get('settings:sentry-reporting', false));
@@ -27,10 +24,6 @@ export const actions = {
 	setDefaultView({ commit }, value) {
 		storage.set('settings:default-view', value);
 		commit('setDefaultView', value);
-	},
-	setHiddenBots({ commit }, value) {
-		storage.set('settings:hidden-bots', value);
-		commit('setHiddenBots', value);
 	},
 	setNicknames({ commit }, value) {
 		storage.set('settings:nicknames', value);
@@ -48,7 +41,6 @@ export const actions = {
 
 export const getters = {
 	defaultView: state => state.defaultView,
-	hiddenBots: state => state.hiddenBots,
 	nicknames: state => state.nicknames,
 	sentryInstalled: state => state.sentryInstalled,
 	sentryReporting: state => state.sentryReporting
