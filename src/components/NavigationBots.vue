bfeb6442e804c810d2e1a0185cd1b87cdd64ad95
MrBurrBurr <31552675+MrBurrBurr@users.noreply.github.com>
2019-01-28 13:37:26 +0100

    Misc

--- src/components/NavigationBots.vue
+++ src/components/NavigationBots.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="bot-cards">
-		<div class="bot-card" :class="[`status--${type}`, { 'bot-card--selected': selectedBots.includes(type) }]" @click.prevent="select(type)" v-for="type in botTypes">
+		<div class="bot-card" :class="[`status--${type}`, { 'bot-card--selected': selectedBots.includes(type) }]" @click.prevent="selectType(type)" v-for="type in botTypes">
 			<fit-text ref="count" :max="1.2" class="bot-card__value">{{ count(type) }}</fit-text>
 			<span class="bot-card__name">{{ $t(`bot-status-${type}`) }}</span>
 		</div>
@@ -41,20 +41,16 @@
 			onTransitionEnd() {
 				this.transitioning = false;
 			},
-			select(type) {
+			selectType(type) {
 				if (this.$route.name !== 'bots') this.$router.push({ name: 'bots' });
 
-				const index = this.selectedBots.indexOf(type);
+				let selectedBots = this.selectedBots.includes(type)
+						? this.selectedBots.filter(botType => botType !== type)
+						: [ ...this.selectedBots, type ];
 
-				if (index >= 0) {
-					this.selectedBots.splice(index, 1);
-				} else {
-					this.selectedBots.push(type);
-				}
+				if (selectedBots.length === this.botTypes.length) selectedBots = [];
 
-				if (this.selectedBots.length === this.botTypes.length) this.selectedBots = [];
-
-				this.$store.dispatch('settings/setSelectedBots', this.selectedBots);
+				this.$store.dispatch('settings/setSelectedBots', selectedBots)
 			}
 		}
 	};
