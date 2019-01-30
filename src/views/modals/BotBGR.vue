bb4b80befe2ae457f63517f32a99972b4da6023a
SKANKHUNTER <31552675+MrBurrBurr@users.noreply.github.com>
2019-01-30 05:35:04 +0100

    Add confirmation to BGR reset (#324)
    * Add confirm to BGR reset
    
    * Test
    
    * Revert "Test"
    
    This reverts commit eade7bff07d1da019b67b231721283366200ac29.

--- src/views/modals/BotBGR.vue
+++ src/views/modals/BotBGR.vue
@@ -6,11 +6,12 @@
 		<h3 class="subtitle" v-if="loading">
 			<font-awesome-icon icon="spinner" size="lg" spin></font-awesome-icon>
 		</h3>
-		<bgr-status v-if="!loading && state === 'input'" :used-keys="usedKeys" :unused-keys="unusedKeys" @reset="onReset" @show-unused="state = 'unusedKeys'" @show-used="state = 'usedKeys'"></bgr-status>
+		<bgr-status v-if="!loading && state === 'input'" :used-keys="usedKeys" :unused-keys="unusedKeys" @reset="showReset" @show-unused="state = 'unusedKeys'" @show-used="state = 'usedKeys'"></bgr-status>
 
 		<keep-alive>
 			<bgr-input v-if="state === 'input'" @check="onCheck"></bgr-input>
 			<bgr-check v-if="state === 'check'" :keys="keys" :title="$t('bgr-check', { n: foundKeysCount })" :bot="bot" :confirming="confirming" @confirm="onConfirm" @cancel="onCancel"></bgr-check>
+			<bgr-reset v-if="state === 'reset'" :title="$t('bgr-reset')" :resetting="resetting" @reset="onReset" @cancel="onCancel"></bgr-reset>
 			<bgr-summary v-if="state === 'summary'" :keys="summaryKeys" :title="$t('bgr-summary-success', { n: addedKeysCount })" @back="$parent.close()"></bgr-summary>
 			<bgr-summary v-if="state === 'usedKeys'" :keys="usedKeys" :title="$t('bgr-used-keys')" @back="state = 'input'"></bgr-summary>
 			<bgr-summary v-if="state === 'unusedKeys'" :keys="unusedKeys" :title="$t('bgr-unused-keys')" @back="state = 'input'"></bgr-summary>
@@ -21,6 +22,7 @@
 <script>
 	import BgrCheck from '../../components/BGR/Check.vue';
 	import BgrInput from '../../components/BGR/Input.vue';
+	import BgrReset from '../../components/BGR/Reset.vue';
 	import BgrStatus from '../../components/BGR/Status.vue';
 	import BgrSummary from '../../components/BGR/Summary.vue';
 
@@ -28,11 +30,12 @@
 
 	export default {
 		name: 'bot-bgr',
-		components: { BgrStatus, BgrInput, BgrCheck, BgrSummary },
+		components: { BgrCheck, BgrInput, BgrReset, BgrStatus, BgrSummary },
 		data() {
 			return {
 				loading: true,
 				confirming: false,
+				resetting: false,
 				state: 'input',
 				unusedKeys: {},
 				usedKeys: {},
@@ -91,10 +94,20 @@
 			onCancel() {
 				this.state = 'input';
 			},
+			showReset() {
+				this.state = 'reset';
+			},
 			async onReset() {
-				await this.$http.del(`bot/${this.bot.name}/GamesToRedeemInBackground`);
-				this.unusedKeys = {};
-				this.usedKeys = {};
+				this.resetting = true;
+
+				try {
+					await this.$http.del(`bot/${this.bot.name}/GamesToRedeemInBackground`);
+					this.unusedKeys = {};
+					this.usedKeys = {};
+				} finally {
+					this.resetting = false;
+					this.state = 'input';
+				}
 			}
 		}
 	};
