b1a250857dcadc39af79e22f93d978f5a92a4fb9
MrBurrBurr <31552675+MrBurrBurr@users.noreply.github.com>
2019-01-30 04:25:23 +0100

    Add confirm to BGR reset

---
+++ src/components/BGR/Reset.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="bgr__reset">
+		<h3 class="subtitle">{{ title }}</h3>
+
+		<div class="form-item">
+			<div class="form-item__buttons form-item__buttons--center">
+				<button class="button button--cancel" @click="$emit('reset')">
+					<font-awesome-icon icon="spinner" v-if="resetting" spin></font-awesome-icon>
+					<span v-else>{{ $t('reset') }}</span>
+				</button>
+
+				<button class="button button--confirm" @click="$emit('cancel')" :key="'cancel'">{{ $t('cancel') }}</button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			resetting: Boolean,
+			title: String
+		},
+		name: 'bgr-reset'
+	};
+</script>
