8e81bcbe5295f4d97661fd2399b8184472036749
SKANKHUNTER <31552675+MrBurrBurr@users.noreply.github.com>
2019-02-25 22:08:05 +0100

    Update ESLint rules (#488)

--- src/utils/timeDifference.js
+++ src/utils/timeDifference.js
@@ -3,8 +3,8 @@ export default function timeDifference(startTime, endTime = new Date()) {
 
 	return {
 		seconds: Math.floor(difference % 60),
-		minutes: Math.floor(difference / 60 % 60),
-		hours: Math.floor(difference / (60 * 60) % 24),
+		minutes: Math.floor((difference / 60) % 60),
+		hours: Math.floor((difference / (60 * 60)) % 24),
 		days: Math.floor(difference / (24 * 60 * 60))
 	};
 }
