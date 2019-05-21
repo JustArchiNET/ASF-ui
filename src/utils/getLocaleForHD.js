1fd8b34bd0611960a52b9141a0e80ccc231c05f2
MrBurrBurr <31552675+MrBurrBurr@users.noreply.github.com>
2019-05-21 13:26:38 +0200

    Misc

--- src/utils/getLocaleForHD.js
+++ src/utils/getLocaleForHD.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
+import humanizeDuration from 'humanize-duration';
 
 export default function getLocaleForHD() {
-	const { locale } = Vue.i18n;
-
+	const supportedLanguages = humanizeDuration.getSupportedLanguages();
+	const { locale, noRegionalLocale } = Vue.i18n;
 	if (locale === 'zh-CN') return 'zh_CN';
 	if (locale === 'zh-TW') return 'zh_TW';
 	if (locale === 'zh-HK') return 'zh_CN';
-	return Vue.i18n.noRegionalLocale;
+	if (supportedLanguages.includes(noRegionalLocale)) return noRegionalLocale;
+	return 'en';
 }
