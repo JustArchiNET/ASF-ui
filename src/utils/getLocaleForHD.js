a9b7d68049ebe8d23b278d28c31903f03fd533e7
MrBurrBurr <31552675+MrBurrBurr@users.noreply.github.com>
2019-05-21 14:49:40 +0200

    Use switch case

--- src/utils/getLocaleForHD.js
+++ src/utils/getLocaleForHD.js
@@ -4,9 +4,15 @@ import humanizeDuration from 'humanize-duration';
 export default function getLocaleForHD() {
 	const supportedLanguages = humanizeDuration.getSupportedLanguages();
 	const { locale, noRegionalLocale } = Vue.i18n;
-	if (locale === 'zh-CN') return 'zh_CN';
-	if (locale === 'zh-TW') return 'zh_TW';
-	if (locale === 'zh-HK') return 'zh_CN';
-	if (supportedLanguages.includes(noRegionalLocale)) return noRegionalLocale;
-	return 'en';
+
+	switch (locale) {
+		case 'zh-CN':
+			return 'zh_CN';
+		case 'zh-TW':
+		case 'zh-HK':
+			return 'zh_TW';
+		default:
+			if (supportedLanguages.includes(noRegionalLocale)) return noRegionalLocale;
+			return 'en';
+	}
 }
