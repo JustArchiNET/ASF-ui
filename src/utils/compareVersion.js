11e36b48c3817c8a6b788a4eea41c762793bdba9
Arkadiusz Sygulski <arkadiusz@sygulski.pl>
2019-08-11 12:19:34 +0200

    Make the version comparison work when version lengths differ

--- src/utils/compareVersion.js
+++ src/utils/compareVersion.js
@@ -1,10 +1,15 @@
 export default function compareVersion(a, b) {
-	const aValues = a.split('.');
-	const bValues = b.split('.');
+	const aValues = a.split('.').map(v => parseInt(v, 10));
+	const bValues = b.split('.').map(v => parseInt(v, 10));
 
-	for (let i = 0; i < aValues.length; ++i) {
-		if (aValues[i] === bValues[i]) continue;
-		return aValues[i] > bValues[i] ? 1 : -1;
+	const versionLength = Math.max(aValues.length, bValues.length)
+
+	for (let i = 0; i < versionLength; ++i) {
+		const aValue = aValues[i] || 0;
+		const bValue = bValues[i] || 0;
+
+		if (aValue === bValue) continue;
+		return aValue > bValue ? 1 : -1;
 	}
 
 	return 0;
