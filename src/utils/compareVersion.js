fa83511caba75594975ec0734602bbaebe357f36
SKANKHUNTER <31552675+MrBurrBurr@users.noreply.github.com>
2019-11-21 20:54:49 +0100

    Misc

--- src/utils/compareVersion.js
+++ src/utils/compareVersion.js
@@ -2,7 +2,7 @@ export default function compareVersion(a, b) {
 	const aValues = a.split('.').map(v => parseInt(v, 10));
 	const bValues = b.split('.').map(v => parseInt(v, 10));
 
-	const versionLength = Math.max(aValues.length, bValues.length)
+	const versionLength = Math.max(aValues.length, bValues.length);
 
 	for (let i = 0; i < versionLength; ++i) {
 		const aValue = aValues[i] || 0;
