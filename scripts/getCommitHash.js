b559b3a876bae63fe46742dec06d62b4b12d70d4
Arkadiusz Sygulski <arkadiusz@sygulski.pl>
2020-06-24 20:45:32 +0200

    Add missing `return` to `getCommitHash`

--- scripts/getCommitHash.js
+++ scripts/getCommitHash.js
@@ -2,7 +2,7 @@ const { execSync } = require('child_process');
 
 module.exports = function getCommitHash() {
 	try {
-		execSync('git rev-parse --short HEAD').toString();
+		return execSync('git rev-parse --short HEAD').toString();
 	} catch (err) {
 		return '0000000';
 	}
