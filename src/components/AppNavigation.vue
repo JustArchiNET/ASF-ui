229cf07ef899103f2337c9ae33f09a5f70e04b03
SKANKHUNTER <31552675+MrBurrBurr@users.noreply.github.com>
2020-06-24 20:40:27 +0200

    Add empty plugin view

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -7,6 +7,7 @@
 				<navigation-link :name="$t('commands')" icon="laptop" :to="{ name: 'commands' }"></navigation-link>
 				<navigation-link :name="$t('log')" icon="file-alt" :to="{ name: 'log' }"></navigation-link>
 				<navigation-link :name="$t('releases')" icon="code-branch" :to="{ name: 'releases' }"></navigation-link>
+				<navigation-link :name="$t('plugins')" icon="puzzle-piece" :to="{ name: 'plugins' }"></navigation-link>
 			</div>
 
 			<div class="navigation-category">
---
+++ src/components/AsfPlugins.vue
@@ -0,0 +1,14 @@
+<template>
+</template>
+
+<script>
+	export default {
+		name: 'asf-plugins',
+		data() {
+			return {
+				loading: true,
+				plugins: []
+			};
+		},
+	};
+</script>
