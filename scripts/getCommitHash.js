e6bb0972f994ec139a1674efb4a8b5ce50a2afcb
Łukasz Domeradzki <JustArchi@JustArchi.net>
2020-10-06 17:02:14 +0200

    Ensure that getCommitHash() always returns 7 chars (#1116)

--- scripts/getCommitHash.js
+++ scripts/getCommitHash.js
@@ -2,7 +2,7 @@ const { execSync } = require('child_process');
 
 module.exports = function getCommitHash() {
   try {
-    return execSync('git rev-parse --short HEAD').toString();
+    return execSync('git rev-parse --short=7 HEAD').toString();
   } catch (err) {
     return '0000000';
   }
