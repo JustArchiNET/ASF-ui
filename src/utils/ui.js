46885878d2a837937cf555741f8fd541b9db1288
SKANKHUNTER <31552675+MrBurrBurr@users.noreply.github.com>
2020-11-02 23:03:39 +0100

    Only send one request per hour to 'www/send' (#1170)
    * Limit requests to asf repo to 1h/request
    
    * Attempt to satisfy codeclimate
    
    * codeclimat will love me now
    
    * Misc

--- src/utils/ui.js
+++ src/utils/ui.js
@@ -1,17 +1,26 @@
 import * as http from '../plugins/http';
 import { state as asf, UPDATECHANNEL } from '../store/modules/asf';
-import { set } from './storage';
+import { set, get } from './storage';
 
 export const ui = { gitCommitHash: APP_HASH };
 
-export async function newReleaseAvailable() {
+async function isReleaseAvailable() {
   const updateChannel = (asf.updateChannel === UPDATECHANNEL.EXPERIMENTAL) ? 'releases' : 'releases/latest';
-  const latestReleaseRaw = await http.post('www/send', { url: `https://api.github.com/repos/JustArchiNET/ArchiSteamFarm/${updateChannel}` });
-  const latestReleaseVersion = JSON.parse(latestReleaseRaw);
+  const response = await http.post('www/send', { url: `https://api.github.com/repos/JustArchiNET/ArchiSteamFarm/${updateChannel}` });
+  set('last-checked-for-update', Date.now());
 
-  if (!latestReleaseVersion) return;
+  const latestRelease = JSON.parse(response);
+  if (!latestRelease) return false;
 
-  const latestVersion = (asf.updateChannel === UPDATECHANNEL.EXPERIMENTAL) ? latestReleaseVersion[0].tag_name : latestReleaseVersion.tag_name;
+  const latestVersion = (asf.updateChannel === UPDATECHANNEL.EXPERIMENTAL) ? latestRelease[0].tag_name : latestRelease.tag_name;
   set('latest-release', latestVersion);
+
   return (latestVersion > asf.version);
+}
+
+export async function newReleaseAvailable() {
+  const lastChecked = get('last-checked-for-update');
+  if (lastChecked && (lastChecked > (Date.now() - 60 * 60 * 1000))) return false;
+
+  return isReleaseAvailable();
 }
