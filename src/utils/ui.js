2b941d39926e92c9c6f817d21b43b758d29e0d9f
SKANKHUNTER <31552675+MrBurrBurr@users.noreply.github.com>
2020-11-02 22:43:31 +0100

    codeclimat will love me now

--- src/utils/ui.js
+++ src/utils/ui.js
@@ -4,22 +4,23 @@ import { set, get } from './storage';
 
 export const ui = { gitCommitHash: APP_HASH };
 
+async function isReleaseAvailable() {
+  const updateChannel = (asf.updateChannel === UPDATECHANNEL.EXPERIMENTAL) ? 'releases' : 'releases/latest';
+  const latestReleaseRaw = await http.post('www/send', { url: `https://api.github.com/repos/JustArchiNET/ArchiSteamFarm/${updateChannel}` });
+  set('last-checked-for-update', Date.now());
+
+  const latestReleaseVersion = JSON.parse(latestReleaseRaw);
+  if (!latestReleaseVersion) return false;
+
+  const latestVersion = (asf.updateChannel === UPDATECHANNEL.EXPERIMENTAL) ? latestReleaseVersion[0].tag_name : latestReleaseVersion.tag_name;
+  set('latest-release', latestVersion);
+
+  return (latestVersion > asf.version);
+}
+
 export async function newReleaseAvailable() {
-  try {
-    const lastChecked = get('last-checked-for-update');
-    if (lastChecked && (lastChecked > (Date.now() - 60 * 60 * 1000))) return false;
+  const lastChecked = get('last-checked-for-update');
+  if (lastChecked && (lastChecked > (Date.now() - 60 * 60 * 1000))) return false;
 
-    const updateChannel = (asf.updateChannel === UPDATECHANNEL.EXPERIMENTAL) ? 'releases' : 'releases/latest';
-    const latestReleaseRaw = await http.post('www/send', { url: `https://api.github.com/repos/JustArchiNET/ArchiSteamFarm/${updateChannel}` });
-    set('last-checked-for-update', Date.now());
-
-    const latestReleaseVersion = JSON.parse(latestReleaseRaw);
-    if (!latestReleaseVersion) return false;
-
-    const latestVersion = (asf.updateChannel === UPDATECHANNEL.EXPERIMENTAL) ? latestReleaseVersion[0].tag_name : latestReleaseVersion.tag_name;
-    set('latest-release', latestVersion);
-    return (latestVersion > asf.version);
-  } catch (err) {
-    console.warn(err.message);
-  }
+  return isReleaseAvailable();
 }
