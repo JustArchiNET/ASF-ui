53390bd1f25df25deb6b78e8c2112a2e5dc9d015
FREDERICK <github@FREDERICK.win>
2021-03-09 08:42:01 +0100

    Don't abuse api/www/send in ui.js

--- src/utils/ui.js
+++ src/utils/ui.js
@@ -7,20 +7,17 @@ export const ui = { gitCommitHash: APP_HASH };
 
 export async function isReleaseAvailable() {
   const lastChecked = get('last-checked-for-update');
+
   if (lastChecked && (lastChecked > (Date.now() - 60 * 60 * 1000))) {
     const latestCachedVersion = get('latest-release');
     return (latestCachedVersion > asf.version);
   }
 
-  const updateChannel = (asf.updateChannel === UPDATECHANNEL.EXPERIMENTAL) ? 'releases' : 'releases/latest';
-  const response = await http.post('www/send', { url: `https://api.github.com/repos/JustArchiNET/ArchiSteamFarm/${updateChannel}` });
+  const endpoint = (asf.updateChannel === UPDATECHANNEL.EXPERIMENTAL) ? 'www/github/release' : 'www/github/release/latest';
+  const response = await http.get(endpoint);
+
+  set('latest-release', response.Version);
   set('last-checked-for-update', Date.now());
 
-  const latestRelease = JSON.parse(response);
-  if (!latestRelease) return false;
-
-  const latestVersion = (asf.updateChannel === UPDATECHANNEL.EXPERIMENTAL) ? latestRelease[0].tag_name : latestRelease.tag_name;
-  set('latest-release', latestVersion);
-
-  return (latestVersion > asf.version);
+  return (response.Version > asf.version);
 }
