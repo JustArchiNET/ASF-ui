93cc67ea5681dfda4df6e21ad37553e04c416617
FREDERICK <github@FREDERICK.win>
2021-03-16 17:56:05 +0100

    Misc formatting

--- src/plugins/icons.js
+++ src/plugins/icons.js
@@ -2,20 +2,22 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 import {
-  faWrench, faBars, faLaptop, faUsers, faFileAlt, faTachometerAlt, faPowerOff, faPause, faCogs, faClock,
-  faTimesCircle, faCheckCircle, faEdit, faTimes, faSquare, faMoon, faPalette, faPlay, faQuestion, faPlus,
-  faSpinner, faKey, faTrash, faCloudDownloadAlt, faSignOutAlt, faAngleDown, faLanguage, faGamepad, faClone,
-  faCalendarCheck, faLock, faBookOpen, faExclamation, faCodeBranch, faHourglassEnd, faHourglassHalf, faHourglassStart,
-  faRedoAlt, faClipboard, faPuzzlePiece, faUndoAlt, faEye, faEyeSlash, faChevronLeft, faChevronRight,
+  faWrench, faBars, faLaptop, faUsers, faFileAlt, faTachometerAlt, faPowerOff, faPause, faCogs,
+  faClock, faTimesCircle, faCheckCircle, faEdit, faTimes, faSquare, faMoon, faPalette, faPlay,
+  faQuestion, faPlus, faSpinner, faKey, faTrash, faCloudDownloadAlt, faSignOutAlt, faAngleDown,
+  faLanguage, faGamepad, faClone, faCalendarCheck, faLock, faBookOpen, faCodeBranch, faHourglassEnd,
+  faHourglassHalf, faHourglassStart, faRedoAlt, faClipboard, faPuzzlePiece, faUndoAlt, faEye,
+  faEyeSlash, faChevronLeft, faChevronRight, faExclamation,
 } from '@fortawesome/free-solid-svg-icons';
 
 import { FontAwesomeIcon, FontAwesomeLayers } from '@fortawesome/vue-fontawesome';
 
-library.add(faWrench, faBars, faLaptop, faUsers, faFileAlt, faTachometerAlt, faPowerOff, faPause, faCogs,
-  faClock, faTimesCircle, faCheckCircle, faEdit, faTimes, faSquare, faMoon, faPalette, faPlay, faQuestion,
-  faPlus, faSpinner, faKey, faTrash, faCloudDownloadAlt, faSignOutAlt, faAngleDown, faLanguage, faGamepad,
-  faClone, faCalendarCheck, faLock, faGithub, faBookOpen, faExclamation, faCodeBranch, faHourglassEnd, faHourglassHalf,
-  faHourglassStart, faRedoAlt, faClipboard, faPuzzlePiece, faUndoAlt, faEye, faEyeSlash, faChevronLeft, faChevronRight);
+library.add(faWrench, faBars, faLaptop, faUsers, faFileAlt, faTachometerAlt, faPowerOff, faPause,
+  faCogs, faClock, faTimesCircle, faCheckCircle, faEdit, faTimes, faSquare, faMoon, faPalette,
+  faPlay, faQuestion, faPlus, faSpinner, faKey, faTrash, faCloudDownloadAlt, faSignOutAlt,
+  faAngleDown, faLanguage, faGamepad, faClone, faCalendarCheck, faLock, faGithub, faBookOpen,
+  faExclamation, faCodeBranch, faHourglassEnd, faHourglassHalf, faHourglassStart, faRedoAlt,
+  faClipboard, faPuzzlePiece, faUndoAlt, faEye, faEyeSlash, faChevronLeft, faChevronRight);
 
 export default {
   install(Vue) {
