6c13336cf306f5d0e383e940fe97e516cb83c71b
FREDERICK <github@FREDERICK.win>
2021-04-06 06:56:15 +0200

    Misc

--- src/utils/downloadConfig.js
+++ src/utils/downloadConfig.js
@@ -4,9 +4,10 @@ function prepareModelToDownload(model) {
   return JSON.stringify(model, null, 2).replace(sPropertyRegex, '"$1":$2');
 }
 
-export default function downloadConfig(config, name) {
+export default function downloadConfig(model, name) {
   const element = document.createElement('a');
-  element.setAttribute('href', `data:text/plain;charset=utf-8,${encodeURIComponent(prepareModelToDownload(config))}`);
+  const config = prepareModelToDownload(model);
+  element.setAttribute('href', `data:text/plain;charset=utf-8,${encodeURIComponent(config)}`);
   element.setAttribute('download', `${name}.json`);
   element.style.display = 'none';
   document.body.appendChild(element);
