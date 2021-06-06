9d44967a98557b45ae278328cd70d54ccc9f7772
FREDERICK <github@FREDERICK.win>
2021-06-06 04:17:26 +0200

    Move styling for pre tags so it can be used globally

--- src/components/AsfReleases.vue
+++ src/components/AsfReleases.vue
@@ -255,18 +255,5 @@
     summary {
       cursor: pointer;
     }
-
-    pre {
-      white-space: pre-wrap;
-      padding: 0.5em 1em;
-      font-size: 90%;
-      background-color: #2d333b;
-      border-radius: 3px;
-      color: var(--color-releases-pre);
-
-      > code {
-        background-color: inherit;
-      }
-    }
 	}
 </style>
