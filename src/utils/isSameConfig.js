c6c620bf315576b3cba914f0d4105ce3134ee7a5
FREDERICK <github@FREDERICK.win>
2021-06-13 23:30:21 +0200

    Fuck you JS
    "...arrays and objects are compared by reference and not by value in JavaScript..."

--- src/utils/isSameConfig.js
+++ src/utils/isSameConfig.js
@@ -1,17 +1,9 @@
+import { isEqual } from 'lodash-es';
+
 export default function isSameConfig(newConfig, oldConfig) {
   // eslint-disable-next-line no-restricted-syntax
   for (const [property] of Object.entries(newConfig)) {
-    if (typeof oldConfig[property] === 'object') {
-      // Vue's observer returns different result for empty object
-      const objectsEmpty = (
-        oldConfig[property]
-        && Object.keys(oldConfig[property]).length === 0
-        && newConfig[property]
-        && Object.keys(newConfig[property]).length === 0);
-      if (objectsEmpty) continue;
-    }
-
-    const foundDifference = oldConfig[property] !== newConfig[property];
+    const foundDifference = !isEqual(oldConfig[property], newConfig[property]);
     if (foundDifference) return false;
   }
 
@@ -19,19 +11,10 @@ export default function isSameConfig(newConfig, oldConfig) {
     if (key.startsWith('s_')) delete oldConfig[key.substr(2)];
   });
 
+  // we want to see if we have some default values set
   // eslint-disable-next-line no-restricted-syntax
   for (const [property] of Object.entries(oldConfig)) {
-    if (typeof oldConfig[property] === 'object') {
-      // Vue's observer returns different result for empty object
-      const objectsEmpty = (
-        oldConfig[property]
-        && Object.keys(oldConfig[property]).length === 0
-        && newConfig[property]
-        && Object.keys(newConfig[property]).length === 0);
-      if (objectsEmpty) continue;
-    }
-
-    const foundDifference = oldConfig[property] !== newConfig[property];
+    const foundDifference = !isEqual(oldConfig[property], newConfig[property]);
     if (foundDifference) return false;
   }
 
