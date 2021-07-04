d953b30f2e74aa95fec74539c131a592ebef4d12
FREDERICK <github@FREDERICK.win>
2021-07-04 04:14:23 +0200

    Further improve isSameConfig detection
    Fixes #1463

--- src/utils/isSameConfig.js
+++ src/utils/isSameConfig.js
@@ -3,10 +3,16 @@ import { isEqual } from 'lodash-es';
 export default function isSameConfig(newConfig, oldConfig) {
   // eslint-disable-next-line no-restricted-syntax
   for (const [property] of Object.entries(newConfig)) {
-    // we want to use lodash for object comparison since JS sucks
-    const foundDifference = (typeof oldConfig[property] === 'object')
-      ? !isEqual(oldConfig[property], newConfig[property])
-      : oldConfig[property] !== newConfig[property];
+    let foundDifference = false;
+
+    if (typeof oldConfig[property] === 'object') {
+      // we want to use lodash for object comparison since JS sucks
+      foundDifference = !isEqual(oldConfig[property], newConfig[property]);
+    } else if (property.startsWith('s_')) {
+      foundDifference = oldConfig[property] !== newConfig[property].toString();
+    } else {
+      foundDifference = oldConfig[property] !== newConfig[property];
+    }
 
     if (foundDifference) return false;
   }
