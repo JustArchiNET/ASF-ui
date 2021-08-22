1b3527ed028381b246a0e19e09bd211650c42d63
FREDERICK <github@FREDERICK.win>
2021-08-22 03:20:13 +0200

    Do not overwrite existing devServer config

--- webpack/config.test.js
+++ webpack/config.test.js
@@ -1,15 +1,13 @@
 const config = require('./config');
 
-config.devServer = {
-  onListening(devServer) {
-    if (!devServer) {
-      throw new Error('webpack-dev-server is not defined');
-    }
+config.devServer.onListening = function stopDevServer(devServer) {
+  if (!devServer) {
+    throw new Error('webpack-dev-server is not defined');
+  }
 
-    devServer.stopCallback(() => {
-      console.log('Stopped dev environment');
-    });
-  },
+  devServer.stopCallback(() => {
+    console.log('Stopped dev server');
+  });
 };
 
 module.exports = config;
