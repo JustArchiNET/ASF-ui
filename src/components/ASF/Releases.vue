5c589d9b602c2439d510ba7b2135094d58cd58fd
FREDERICK <github@FREDERICK.win>
2021-10-15 16:38:58 +0200

    Add comment

--- src/components/ASF/Releases.vue
+++ src/components/ASF/Releases.vue
@@ -31,6 +31,8 @@
   import compareVersion from '../../utils/compareVersion';
   import { UPDATECHANNEL } from '../../store/modules/asf';
 
+  // We are using require here because linkify devs are mad. They are exporting a submodule
+  // already included in the base module with base module as peer dependency. True madness.
   const linkifyHtml = require('linkifyjs/lib/linkify-html');
 
   export default {
