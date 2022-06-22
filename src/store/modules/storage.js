f024fd3da425917930219fdf79ed3e7988b12a57
yuyinws <lyc657508366@gmail.com>
2022-06-22 14:26:59 +0800

    feat: load theme and darkmode from ASF Storage

---
+++ src/store/modules/storage.js
@@ -0,0 +1,54 @@
+import * as http from '../../plugins/http';
+import * as storage from '../../utils/storage';
+
+export const state = {
+  theme: 'blue',
+  darkMode: false,
+};
+
+export const mutations = {
+  changeTheme: (state, theme) => (state.theme = theme),
+  setDarkMode: (state, value) => (state.darkMode = value),
+  toggleDarkMode: state => (state.darkMode = !state.darkMode),
+};
+
+export const actions = {
+  init: async ({ commit }) => {
+    try {
+      // get setting from ASF
+      const response = await http.get('/storage/asfui-settings');
+      const { theme: asfTheme, darkMode: asfDarkmode } = response;
+
+      // get setting from local storage
+      const localTheme = storage.get('layout:theme');
+      const localDarkMode = storage.get('layout:dark-mode');
+
+      // set setting. local > ASF
+      const theme = localTheme ?? asfTheme;
+      const darkMode = localDarkMode ?? asfDarkmode;
+
+      commit('changeTheme', theme);
+      if (typeof darkMode === 'boolean') commit('setDarkMode', darkMode);
+      else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) commit('setDarkMode', true);
+
+      http.post('/storage/asfui-settings', state);
+    } catch (err) {
+      console.warn(err.message);
+    }
+  },
+  changeTheme: ({ commit, state }, _theme) => {
+    commit('changeTheme', _theme);
+    storage.set('layout:theme', _theme);
+    http.post('/storage/asfui-settings', state);
+  },
+  toggleDarkMode: ({ commit, getters }) => {
+    commit('toggleDarkMode');
+    storage.set('layout:dark-mode', getters.darkMode);
+    http.post('/storage/asfui-settings', state);
+  },
+};
+
+export const getters = {
+  theme: state => state.theme,
+  darkMode: state => state.darkMode,
+};
