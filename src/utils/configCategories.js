08ddd7b5af71d4169cb320d106a558f8b74dfd5d
FREDERICK <github@FREDERICK.win>
2022-12-23 22:38:18 +0100

    Update configCategories.js

--- src/utils/configCategories.js
+++ src/utils/configCategories.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 
 const asfCategories = [
   { name: Vue.i18n.translate('basic'), fields: ['SteamOwnerID'] },
-  { name: Vue.i18n.translate('trade'), fields: ['MaxTradeHoldDuration'] },
+  { name: Vue.i18n.translate('trade'), fields: ['MaxTradeHoldDuration', 'FilterBadBots', 'LicenseID'] },
   { name: Vue.i18n.translate('customization'), fields: ['AutoRestart', 'Blacklist', 'CommandPrefix', 'CurrentCulture', 'SteamMessagePrefix'] },
   { name: Vue.i18n.translate('remote-access'), fields: ['Headless', 'IPC', 'IPCPassword', 'IPCPasswordFormat'] },
   { name: Vue.i18n.translate('connection'), fields: ['ConnectionTimeout', 'SteamProtocols', 'WebProxy', 'WebProxyPassword', 'WebProxyUsername'] },
