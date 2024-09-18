55b420505fa563db55eea14367cd7d54e7e4b865
Messansio <andreamessanaj@gmail.com>
2024-09-18 09:28:20 +0200

    Added 1 line into script/getCommitHash.js, solving issue #1116

--- scripts/getCommitHash.js
+++ scripts/getCommitHash.js
@@ -2,7 +2,8 @@ const { execSync } = require('child_process');
 
 module.exports = function getCommitHash() {
   try {
-    return execSync('git rev-parse --short=7 HEAD').toString();
+    let hash = execSync('git rev-parse --short=7 HEAD').toString().trim();
+    return hash.padStart(7, '0');
   } catch (err) {
     return '0000000';
   }
