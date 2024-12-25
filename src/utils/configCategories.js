6be24065cd4904389d94140f4ed1f6738b1f7fbb
FREDERICK <github@FREDERICK.win>
2024-12-25 11:54:54 +0100

    Add `ShutdownIfPossible` to farming bot config category

--- src/utils/configCategories.js
+++ src/utils/configCategories.js
@@ -6,7 +6,7 @@ const asfCategories = [
   { name: Vue.i18n.translate('customization'), fields: ['AutoRestart', 'Blacklist', 'CommandPrefix', 'CurrentCulture', 'SteamMessagePrefix'] },
   { name: Vue.i18n.translate('remote-access'), fields: ['Headless', 'IPC', 'IPCPassword', 'IPCPasswordFormat'] },
   { name: Vue.i18n.translate('connection'), fields: ['ConnectionTimeout', 'SteamProtocols', 'WebProxy', 'WebProxyPassword', 'WebProxyUsername'] },
-  { name: Vue.i18n.translate('farming'), fields: ['FarmingDelay', 'IdleFarmingPeriod', 'MaxFarmingTime', 'MinFarmingDelayAfterBlock'] },
+  { name: Vue.i18n.translate('farming'), fields: ['FarmingDelay', 'IdleFarmingPeriod', 'MaxFarmingTime', 'MinFarmingDelayAfterBlock', 'ShutdownIfPossible'] },
   { name: Vue.i18n.translate('performance'), fields: ['OptimizationMode', 'ConfirmationsLimiterDelay', 'GiftsLimiterDelay', 'InventoryLimiterDelay', 'LoginLimiterDelay', 'WebLimiterDelay'] },
   { name: Vue.i18n.translate('updates'), fields: ['UpdateChannel', 'UpdatePeriod'] },
   { name: Vue.i18n.translate('plugins'), fields: ['PluginsUpdateMode', 'PluginsUpdateList'] },
